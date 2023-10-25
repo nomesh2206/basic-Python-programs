3f9f42155f4ce1302adc6ca224672ec05531e519
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2023-10-25 16:08:07 +0530

    Flatten the given Nested List

---
+++ Flatten_List.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 16:01:20 2023
+
+@author: a
+"""
+
+def flatten_list(nested_list):
+    flattened_list = []
+    for item in nested_list:
+        if isinstance(item, list):
+            flattened_list.extend(flatten_list(item))
+        else:
+            flattened_list.append(item)
+    return flattened_list
+
+# Example nested list
+nested_list = [1, [2, [3, 4]], 5, [6, 7, [8, 9]]]
+
+flattened_list = flatten_list(nested_list)
+print(flattened_list)

0aabb31fe74e587ad0b9d1ce217f0628fe3879de
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2023-10-25 16:09:55 +0530

    Flatten the given Nested List

--- Flatten_List.py
+++ Flatten_List.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 """
 Created on Wed Oct 25 16:01:20 2023
-
-@author: a
+@author: nomesh
 """
 
 def flatten_list(nested_list):
@@ -19,3 +17,6 @@ nested_list = [1, [2, [3, 4]], 5, [6, 7, [8, 9]]]
 
 flattened_list = flatten_list(nested_list)
 print(flattened_list)
+
+Output:
+[1, 2, 3, 4, 5, 6, 7, 8, 9]

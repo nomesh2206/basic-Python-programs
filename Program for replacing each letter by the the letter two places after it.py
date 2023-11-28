c28be25289bcd0bb52f830548723da8ee07f7f54
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2023-11-28 23:58:06 +0530

    Update Program for replacing each letter by the the letter two places after it.py

--- Program for replacing each letter by the the letter two places after it.py
+++ Program for replacing each letter by the the letter two places after it.py
@@ -1,3 +1,4 @@
+Solution 1 :
 input = input("Enter a string to un-ROT2.")
 
 str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -9,3 +10,12 @@ print (input.translate(t))
 
 -------------------------------------------------------------------
 
+Solution 2 :
+We can also use a pair of functions that would help us make the change:
+
+ord(): character to integer
+chr(): integer to character
+
+input = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle grgl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+print(''.join([chr(((ord(s) + 2) - ord('a')) % 26 + ord('a')) if s >= 'a' and s <= 'z' else s for s in input]))

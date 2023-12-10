d6a6f66aea12dbeba13723c4ac1cf6001749b06e
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2023-12-10 16:35:44 +0530

    Update Smith_Number.py
    Static methods are often used for utility functions that are related to the class but don't depend on the instance state. In the provided Python code, the @staticmethod decorator is used for methods such as smith_num, prime_factor_sum, and digit_sum in the Solution class.

--- Smith_Number.py
+++ Smith_Number.py
@@ -1,22 +1,32 @@
 class Solution:
-    def prime_factors(self, n):
-        factors = []
-        divisor = 2
-
-        while n > 1:
-            while n % divisor == 0:
-                factors.append(divisor)
-                n //= divisor
-            divisor += 1
-
-        return factors
-        
-    def digit_sum(self, n):
-        return sum(int(digit) for digit in str(n))
+    @staticmethod
+    def smithNum(n):
+        if Solution.digit_sum(n) == Solution.prime_factor_sum(n):
+            return 1
+        return 0
     
-    def is_smith_number(self, n):
-        factors = self.prime_factors(n)
-        digit_sum_original = self.digit_sum(n)
-        digit_sum_factors = sum(self.digit_sum(factor) for factor in factors)
-        
-        return digit_sum_original == digit_sum_factors
+    @staticmethod
+    def prime_factor_sum(n):
+        count = 0
+        res = 0
+        i = 2
+        while n > 1:
+            if n % i == 0:
+                count += 1
+            if n % i == 0:
+                if i > 10:
+                    res += Solution.digit_sum(i)
+                else:
+                    res += i
+                n = n // i
+            else:
+                i += 1
+        return res if count > 1 else 0
+
+    @staticmethod
+    def digit_sum(x):
+        res = 0
+        while x != 0:
+            res += x % 10
+            x = x // 10
+        return res

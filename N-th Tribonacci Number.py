11e4658ff249da74ace8bb149ac6dffb66f8f572
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2023-12-21 00:12:30 +0530

    Update N-th Tribonacci Number.py

--- N-th Tribonacci Number.py
+++ N-th Tribonacci Number.py
@@ -1,15 +1,13 @@
-def nthTribonacci(n):
-    dp = [-1]*21
-    dp[0] = 0
-    dp[1] = dp[2] = 1
-    def tribo(n):
-        if(n==0 or n==1):
-            return 0
-        if(n==2):
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        A = [0]*(n+1)
+        if n == 0 or n == 1:
+            return n
+        if n == 2:
             return 1
-        if(dp[n]!=-1):
-            return dp[n]
-        nthNumber = nthTribonacci(n-1)+nthTribonacci(n-2)+nthTribonacci(n-3)
-        dp[n] = nthNumber
-        return nthNumber
-    return tribo(n)
+        A[0] = 0
+        A[1] = 1
+        A[2] = 1
+        for i in range(3, n+1):
+            A[i] = A[i-1] + A[i-2] + A[i-3]
+        return A[n]

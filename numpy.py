8bfd4493c2ab3525c6a3795c52cc629a14e36cc3
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2024-01-17 00:18:02 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,3 +1,13 @@
+##Create 1-d Numpy Array
 lst = [1,2,3,4,5]
-arr = np.array[lst]
+arr = np.array(lst)
 print(arr)
+
+##Create n-d Numpy Array
+a = np.random.randint(1,12,(6,5))
+print(a)
+
+##Sorting Numpy array
+np.sort(a)  --> (for nd-array, it will sort row-wise)
+
+np.sort(a,axis=0)  --> (for nd-array, it will sort column-wise)

3e5a495b20d3b537762f973753904995f057cbc4
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2024-01-19 20:29:16 +0530

    Update Bubble_sort_recursive.py

--- Sorting Algorithms/Bubble_sort_recursive.py
+++ Sorting Algorithms/Bubble_sort_recursive.py
@@ -1,14 +1,14 @@
 def bubble_sort_recursive(collection: list[Any]) -> list[Any]:
-    """It is similar iterative bubble sort but recursive.
-    :param collection: mutable ordered sequence of elements
-    :return: the same list in ascending order
+    """ It is similar to iterative bubble sort but recursive.
+    : param collection: mutable ordered sequence of elements
+    : return: the same list in ascending order
     """
   
     length = len(collection)
-    swapped = False
+    swapped = True
     for i in range(length - 1):
         if collection[i] > collection[i + 1]:
             collection[i], collection[i + 1] = collection[i + 1], collection[i]
-            swapped = True
+            swapped = False
 
-    return collection if not swapped else bubble_sort_recursive(collection)
+    return collection if swapped else bubble_sort_recursive(collection)

23738c6a70e588bfc11f6e0f1a4f14ec22bff19f
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2024-02-09 00:21:16 +0530

    Create Adding-nodes-at-the-end.py

---
+++ Linked_List/Adding-nodes-at-the-end.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+# Example usage:
+my_list = LinkedList()
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.display()

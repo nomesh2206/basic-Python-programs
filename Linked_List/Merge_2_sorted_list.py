df63734ea337bba5784427aec93a88334db30fc2
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2024-02-23 20:31:27 +0530

    Create Merge_2_sorted_list.py

---
+++ Linked_List/Merge_2_sorted_list.py
@@ -0,0 +1,38 @@
+class ListNode:
+    def __init__(self, value=0, next=None):
+        self.value = value
+        self.next = next
+
+def merge_sorted_lists(list1, list2):
+    dummy = ListNode()
+    current = dummy
+
+    while list1 is not None and list2 is not None:
+        if list1.value < list2.value:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+
+    current.next = list1 or list2
+
+    return dummy.next
+
+# Creating example linked lists
+list1 = ListNode(1)
+list1.next = ListNode(3)
+list1.next.next = ListNode(5)
+
+list2 = ListNode(2)
+list2.next = ListNode(4)
+list2.next.next = ListNode(6)
+
+# Merging the lists
+merged_list = merge_sorted_lists(list1, list2)
+
+# Printing the merged list
+while merged_list:
+    print(merged_list.value, end=" ")
+    merged_list = merged_list.next

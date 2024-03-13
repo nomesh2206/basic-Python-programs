87466a3c564a1d1b9f2522fbe413a7a14eff7d21
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2024-03-13 15:17:30 +0530

    Create QuickSort.py

---
+++ Sorting Algorithms/QuickSort.py
@@ -0,0 +1,34 @@
+class Solution:
+    #Function to sort a list using quick sort algorithm.
+    def quickSort(self,arr,low,high):
+        # code here
+        if low < high :
+            p = self.partition(arr, low, high)
+            self.quickSort(arr, low, p)
+            self.quickSort(arr, p+1, high)
+    
+    def partition(self,arr,low,high):
+        # code here
+        pivot = arr[low]
+        i = low - 1
+        j = high + 1
+        
+        while True :
+            i = i+1
+            while arr[i] < pivot :
+                i = i+1
+            
+            j = j-1
+            while arr[j] > pivot :
+                j = j-1
+            
+            if i >= j:
+                return j
+            
+            else :
+                arr[i], arr[j] = arr[j], arr[i]
+
+
+
+# we use Hoare's Partitioning in this method
+            
